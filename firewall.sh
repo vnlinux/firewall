ebd5543326a561750584171e2bfc11fabd2463c4
kien kennedy <kiennd@gmail.com>
2014-12-13 22:05:04 +0700

    make it clean and clear

--- firewall.sh
+++ firewall.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-### Declare some system variables
+# declare some system variables
 iptables="/sbin/iptables"
-EXT_IF=$(/sbin/ip route | grep default | awk '{print $5}') # external network interface
-INT_IF=$(/sbin/ip link show | grep "state UP" | grep -v $EXT_IF | awk '{print $2}' | cut -d':' -f1) # all internal network interfaces
+ext_if=$(/sbin/ip route | grep default | awk '{print $5}') # external network interface
+int_if=$(/sbin/ip link show | grep "state UP" | grep -v $ext_if | awk '{print $2}' | cut -d':' -f1) # all internal network interfaces
 
 network_addr=$(/sbin/ip route | grep default | awk '{print $3}' | cut -d"." -f1-3)
 broadcast_addr="$network_addr.255"
 
-### Declare some options to choose
+# declare some options to choose
 LAN_ALLOW="1" # this will set allow all connection from LAN
 BLACKLIST_BLOCK="1" # enable block ips from blacklist
 WHITELIST_ALLOW="1" # enable allow ips from whitelist
 
-### List incoming and outgoing TCP & UDP ports (22 is mandatory, not list here)
+# list incoming and outgoing TCP & UDP ports (22 is mandatory, not list here)
 incoming_tcp="80,443"
 incoming_udp="53"
 outgoing_tcp="22,53,80"
 outgoing_udp="53,123"
 
-### File
+# file
 black_list="blacklist.txt"
 white_list="whitelist.txt"
 
-### Check if file is found
+# check if file is found
 if [ $BLACKLIST_BLOCK = "1" ] && [ ! -f $black_list ]; then
 	echo "File $black_list not found."
 	exit 1
@@ -35,10 +35,9 @@ if [ $WHITELIST_ALLOW = "1" ] && [ ! -f "$white_list" ]; then
 fi
 
 ### STARTING FIREWALL
-echo "STARTING FIREWALL:"
+echo -n "Starting firewall: "
 
 # tuning network protection
-echo "-> tuning network protection"
 echo 1 > /proc/sys/net/ipv4/tcp_syncookies                          # enable TCP SYN cookie protection
 echo 0 > /proc/sys/net/ipv4/conf/all/log_martians                   # log packets with impossible addresses to kernel log
 echo 0 > /proc/sys/net/ipv4/conf/all/accept_source_route            # disable IP Source routing
@@ -46,8 +45,7 @@ echo 0 > /proc/sys/net/ipv4/conf/all/accept_redirects               # disable IC
 echo 1 > /proc/sys/net/ipv4/conf/all/rp_filter                      # enable IP spoofing protection
 echo 1 > /proc/sys/net/ipv4/icmp_echo_ignore_broadcasts             # ignore echo broadcast requests to prevent being part of smurf attacks
 
-### Delete all existing rules
-echo "-> flush all existing rules"
+# delete all existing rules
 $iptables -F
 $iptables -X
 $iptables -t nat -F
@@ -61,31 +59,27 @@ $iptables -P FORWARD DROP
 $iptables -P OUTPUT  DROP
 
 # drop broadcast (do not log)
-$iptables -A INPUT -i $EXT_IF -d 255.255.255.255 -j DROP
-$iptables -A INPUT -i $EXT_IF -d $broadcast_addr -j DROP
+$iptables -A INPUT -i $ext_if -d 255.255.255.255 -j DROP
+$iptables -A INPUT -i $ext_if -d $broadcast_addr -j DROP
 
-### Allow allow loopback
-echo "-> allow loopback"
+# allow allow loopback
 $iptables -A INPUT  -i lo -j ACCEPT
 $iptables -A OUTPUT -o lo -j ACCEPT
 
-### Allow LAN connection
+# allow LAN connection
 if [ "$LAN_ALLOW" = "1" ]; then
-	echo "-> allow all LAN connection"
-	for eth in $INT_IF; do
+	for eth in $int_if; do
 		$iptables -A INPUT -i $eth -j ACCEPT
 		$iptables -A OUTPUT -o $eth -j ACCEPT
 	done
 fi
 
-### Allow incoming SSH
-echo "-> allow incoming SSH"
-$iptables -A INPUT 	-i $EXT_IF -p tcp --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
-$iptables -A OUTPUT 	-o $EXT_IF -p tcp --sport 22 -m state --state ESTABLISHED -j ACCEPT
+# allow incoming SSH
+$iptables -A INPUT 	-i $ext_if -p tcp --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
+$iptables -A OUTPUT -o $ext_if -p tcp --sport 22 -m state --state ESTABLISHED -j ACCEPT
 
-### Allow good ip from whitelist file
+# allow good ip from whitelist file
 if [ "$WHITELIST_ALLOW" = "1" ]; then
-	echo "-> allow good ips from whitelist"
 	$iptables -N acceptlist
 	good_ips=$(egrep -v -E "^#|^$" $white_list)
 	for ip in $good_ips; do
@@ -97,9 +91,8 @@ if [ "$WHITELIST_ALLOW" = "1" ]; then
 	$iptables -I FORWARD -j acceptlist
 fi
 
-### Block bad ip from blacklist file
+# block bad ip from blacklist file
 if [ "$BLACKLIST_BLOCK" = "1" ]; then
-	echo "-> block bad ips from blacklist"
 	$iptables -N droplist
 	bad_ips=$(egrep -v -E "^#|^$" $black_list)
 	for ip in $bad_ips; do
@@ -112,41 +105,35 @@ if [ "$BLACKLIST_BLOCK" = "1" ]; then
 	$iptables -I FORWARD -j droplist
 fi
 
-### Allow incoming TCP
-echo "-> allow incoming $incoming_tcp"
-$iptables -A INPUT 	-i $EXT_IF -p tcp -m multiport --dports $incoming_tcp -m state --state NEW,ESTABLISHED -j ACCEPT
-$iptables -A OUTPUT -o $EXT_IF -p tcp -m multiport --sports $incoming_tcp -m state --state ESTABLISHED -j ACCEPT
+# allow incoming TCP
+$iptables -A INPUT 	-i $ext_if -p tcp -m multiport --dports $incoming_tcp -m state --state NEW,ESTABLISHED -j ACCEPT
+$iptables -A OUTPUT -o $ext_if -p tcp -m multiport --sports $incoming_tcp -m state --state ESTABLISHED -j ACCEPT
 
-### Allow incoming UDP
-echo "-> allow incoming $incoming_udp"
-$iptables -A INPUT 	-i $EXT_IF -p udp -m multiport --dports $incoming_udp -m state --state NEW,ESTABLISHED -j ACCEPT
-$iptables -A OUTPUT -o $EXT_IF -p udp -m multiport --sports $incoming_udp -m state --state ESTABLISHED -j ACCEPT
+# allow incoming UDP
+$iptables -A INPUT 	-i $ext_if -p udp -m multiport --dports $incoming_udp -m state --state NEW,ESTABLISHED -j ACCEPT
+$iptables -A OUTPUT -o $ext_if -p udp -m multiport --sports $incoming_udp -m state --state ESTABLISHED -j ACCEPT
 
-### Allow outgoing TCP
-echo "-> allow outgoing $outgoing_tcp"
-$iptables -A OUTPUT -o $EXT_IF -p tcp -m multiport --dports $outgoing_tcp -m state --state NEW,ESTABLISHED -j ACCEPT
-$iptables -A INPUT 	-i $EXT_IF -p tcp -m multiport --sports $outgoing_tcp -m state --state ESTABLISHED -j ACCEPT
+# allow outgoing TCP
+$iptables -A OUTPUT -o $ext_if -p tcp -m multiport --dports $outgoing_tcp -m state --state NEW,ESTABLISHED -j ACCEPT
+$iptables -A INPUT 	-i $ext_if -p tcp -m multiport --sports $outgoing_tcp -m state --state ESTABLISHED -j ACCEPT
 
-### Allow outgoing UDP
-echo "-> allow outgoing $outgoing_udp"
-$iptables -A OUTPUT -o $EXT_IF -p udp -m multiport --dports $outgoing_udp -m state --state NEW,ESTABLISHED -j ACCEPT
-$iptables -A INPUT 	-i $EXT_IF -p udp -m multiport --sports $outgoing_udp -m state --state ESTABLISHED -j ACCEPT
+# allow outgoing UDP
+$iptables -A OUTPUT -o $ext_if -p udp -m multiport --dports $outgoing_udp -m state --state NEW,ESTABLISHED -j ACCEPT
+$iptables -A INPUT 	-i $ext_if -p udp -m multiport --sports $outgoing_udp -m state --state ESTABLISHED -j ACCEPT
 
-### Make sure to drop bad packages
-echo "-> drop bad packages"
+# make sure to drop bad packages
 $iptables -A INPUT -f -j DROP # Drop packages with incoming fragments
 $iptables -A INPUT -p tcp --tcp-flags ALL ALL -j DROP # Drop incoming malformed XMAS packets
 $iptables -A INPUT -p tcp --tcp-flags ALL NONE -j DROP # Drop all NULL packets
 $iptables -A INPUT -p tcp ! --syn -m state --state NEW -j DROP # Drop all new connection are not SYN packets
 
-### ICMP (PING) - Ping flood projection 1 per second
-echo "-> allow ping"
+# ping flood projection 5 per second
 $iptables -A INPUT 	-p icmp -m limit --limit 5/s -j ACCEPT
 $iptables -A OUTPUT -p icmp -m limit --limit 5/s -j ACCEPT
 $iptables -A INPUT 	-p icmp -j DROP
 $iptables -A OUTPUT	-p icmp -j DROP
 
-### Log all the rest before dropping
+# log all the rest before dropping
 $iptables -A INPUT   -j LOG --log-prefix "IN: "
 $iptables -A INPUT   -j DROP
 $iptables -A OUTPUT  -j LOG --log-prefix "OU: "
@@ -155,4 +142,4 @@ $iptables -A FORWARD -j LOG --log-prefix "FW: "
 $iptables -A FORWARD -j DROP
 
 ### End
-echo "DONE."
+echo "OK."
