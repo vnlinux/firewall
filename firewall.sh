39abc77f8260fffe1920d444dcebae82c36c0f3f
kien kennedy <kiennd@gmail.com>
2014-12-13 21:57:08 +0700

    tunning sysctl.conf, drop broadcast

--- firewall.sh
+++ firewall.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 ### Declare some system variables
-IPT=$(which iptables)
+iptables="/sbin/iptables"
 EXT_IF=$(/sbin/ip route | grep default | awk '{print $5}') # external network interface
-INT_IF=$(ip link show | grep "state UP" | grep -v $EXT_IF | awk '{print $2}' | cut -d':' -f1) # all internal network interfaces
+INT_IF=$(/sbin/ip link show | grep "state UP" | grep -v $EXT_IF | awk '{print $2}' | cut -d':' -f1) # all internal network interfaces
+
+network_addr=$(/sbin/ip route | grep default | awk '{print $3}' | cut -d"." -f1-3)
+broadcast_addr="$network_addr.255"
 
 ### Declare some options to choose
 LAN_ALLOW="1" # this will set allow all connection from LAN
@@ -13,7 +16,7 @@ WHITELIST_ALLOW="1" # enable allow ips from whitelist
 ### List incoming and outgoing TCP & UDP ports (22 is mandatory, not list here)
 incoming_tcp="80,443"
 incoming_udp="53"
-outgoing_tcp="22,53"
+outgoing_tcp="22,53,80"
 outgoing_udp="53,123"
 
 ### File
@@ -34,112 +37,122 @@ fi
 ### STARTING FIREWALL
 echo "STARTING FIREWALL:"
 
-### Set default chain policies
-$IPT -P INPUT DROP
-$IPT -P FORWARD DROP
-$IPT -P OUTPUT DROP
+# tuning network protection
+echo "-> tuning network protection"
+echo 1 > /proc/sys/net/ipv4/tcp_syncookies                          # enable TCP SYN cookie protection
+echo 0 > /proc/sys/net/ipv4/conf/all/log_martians                   # log packets with impossible addresses to kernel log
+echo 0 > /proc/sys/net/ipv4/conf/all/accept_source_route            # disable IP Source routing
+echo 0 > /proc/sys/net/ipv4/conf/all/accept_redirects               # disable ICMP Redirect acceptance
+echo 1 > /proc/sys/net/ipv4/conf/all/rp_filter                      # enable IP spoofing protection
+echo 1 > /proc/sys/net/ipv4/icmp_echo_ignore_broadcasts             # ignore echo broadcast requests to prevent being part of smurf attacks
 
 ### Delete all existing rules
 echo "-> flush all existing rules"
-$IPT -F
-$IPT -X
-$IPT -t nat -F
-$IPT -t nat -X
-$IPT -t mangle -F
-$IPT -t mangle -X
+$iptables -F
+$iptables -X
+$iptables -t nat -F
+$iptables -t nat -X
+$iptables -t mangle -F
+$iptables -t mangle -X
+
+# default policy
+$iptables -P INPUT   DROP
+$iptables -P FORWARD DROP
+$iptables -P OUTPUT  DROP
+
+# drop broadcast (do not log)
+$iptables -A INPUT -i $EXT_IF -d 255.255.255.255 -j DROP
+$iptables -A INPUT -i $EXT_IF -d $broadcast_addr -j DROP
 
 ### Allow allow loopback
 echo "-> allow loopback"
-$IPT -A INPUT  -i lo -j ACCEPT
-$IPT -A OUTPUT -o lo -j ACCEPT
+$iptables -A INPUT  -i lo -j ACCEPT
+$iptables -A OUTPUT -o lo -j ACCEPT
 
 ### Allow LAN connection
 if [ "$LAN_ALLOW" = "1" ]; then
 	echo "-> allow all LAN connection"
 	for eth in $INT_IF; do
-		$IPT -A INPUT -i $eth -j ACCEPT
-		$IPT -A OUTPUT -o $eth -j ACCEPT
+		$iptables -A INPUT -i $eth -j ACCEPT
+		$iptables -A OUTPUT -o $eth -j ACCEPT
 	done
 fi
 
-### Allow current established and related connections
-echo "-> allow current established connections"
-$IPT -A INPUT 	-m state --state RELATED,ESTABLISHED -j ACCEPT 
-$IPT -A OUTPUT 	-m state --state RELATED,ESTABLISHED -j ACCEPT 
-
 ### Allow incoming SSH
 echo "-> allow incoming SSH"
-$IPT -A INPUT 	-i $EXT_IF -p tcp --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
-$IPT -A OUTPUT 	-o $EXT_IF -p tcp --sport 22 -m state --state ESTABLISHED -j ACCEPT
+$iptables -A INPUT 	-i $EXT_IF -p tcp --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
+$iptables -A OUTPUT 	-o $EXT_IF -p tcp --sport 22 -m state --state ESTABLISHED -j ACCEPT
 
 ### Allow good ip from whitelist file
 if [ "$WHITELIST_ALLOW" = "1" ]; then
 	echo "-> allow good ips from whitelist"
-	$IPT -N acceptlist
+	$iptables -N acceptlist
 	good_ips=$(egrep -v -E "^#|^$" $white_list)
 	for ip in $good_ips; do
-		$IPT -A acceptlist -s $ip -j ACCEPT
+		$iptables -A acceptlist -s $ip -j ACCEPT
 	done
 	# insert or append our acceptlist
-	$IPT -I INPUT -j acceptlist
-	$IPT -I OUTPUT -j acceptlist
-	$IPT -I FORWARD -j acceptlist
+	$iptables -I INPUT -j acceptlist
+	$iptables -I OUTPUT -j acceptlist
+	$iptables -I FORWARD -j acceptlist
 fi
 
 ### Block bad ip from blacklist file
 if [ "$BLACKLIST_BLOCK" = "1" ]; then
 	echo "-> block bad ips from blacklist"
-	$IPT -N droplist
+	$iptables -N droplist
 	bad_ips=$(egrep -v -E "^#|^$" $black_list)
 	for ip in $bad_ips; do
-		$IPT -A droplist -s $ip -j LOG --log-prefix "Drop ip in blacklist"
-		$IPT -A droplist -s $ip -j DROP
+		$iptables -A droplist -s $ip -j LOG --log-prefix "Drop ip in blacklist"
+		$iptables -A droplist -s $ip -j DROP
 	done
 	# insert or append our droplist 
-	$IPT -I INPUT -j droplist
-	$IPT -I OUTPUT -j droplist
-	$IPT -I FORWARD -j droplist
+	$iptables -I INPUT -j droplist
+	$iptables -I OUTPUT -j droplist
+	$iptables -I FORWARD -j droplist
 fi
 
 ### Allow incoming TCP
 echo "-> allow incoming $incoming_tcp"
-$IPT -A INPUT 	-i $EXT_IF -p tcp -m multiport --dports $incoming_tcp -m state --state NEW,ESTABLISHED -j ACCEPT
-$IPT -A OUTPUT 	-o $EXT_IF -p tcp -m multiport --sports $incoming_tcp -m state --state ESTABLISHED -j ACCEPT
+$iptables -A INPUT 	-i $EXT_IF -p tcp -m multiport --dports $incoming_tcp -m state --state NEW,ESTABLISHED -j ACCEPT
+$iptables -A OUTPUT -o $EXT_IF -p tcp -m multiport --sports $incoming_tcp -m state --state ESTABLISHED -j ACCEPT
 
 ### Allow incoming UDP
 echo "-> allow incoming $incoming_udp"
-$IPT -A INPUT 	-i $EXT_IF -p udp -m multiport --dports $incoming_udp -m state --state NEW,ESTABLISHED -j ACCEPT
-$IPT -A OUTPUT 	-o $EXT_IF -p udp -m multiport --sports $incoming_udp -m state --state ESTABLISHED -j ACCEPT
+$iptables -A INPUT 	-i $EXT_IF -p udp -m multiport --dports $incoming_udp -m state --state NEW,ESTABLISHED -j ACCEPT
+$iptables -A OUTPUT -o $EXT_IF -p udp -m multiport --sports $incoming_udp -m state --state ESTABLISHED -j ACCEPT
 
 ### Allow outgoing TCP
 echo "-> allow outgoing $outgoing_tcp"
-$IPT -A OUTPUT 	-o $EXT_IF -p tcp -m multiport --dports $outgoing_tcp -m state --state NEW,ESTABLISHED -j ACCEPT
-$IPT -A INPUT 	-i $EXT_IF -p tcp -m multiport --sports $outgoing_tcp -m state --state ESTABLISHED -j ACCEPT
+$iptables -A OUTPUT -o $EXT_IF -p tcp -m multiport --dports $outgoing_tcp -m state --state NEW,ESTABLISHED -j ACCEPT
+$iptables -A INPUT 	-i $EXT_IF -p tcp -m multiport --sports $outgoing_tcp -m state --state ESTABLISHED -j ACCEPT
 
 ### Allow outgoing UDP
 echo "-> allow outgoing $outgoing_udp"
-$IPT -A OUTPUT 	-o $EXT_IF -p udp -m multiport --dports $outgoing_udp -m state --state NEW,ESTABLISHED -j ACCEPT
-$IPT -A INPUT 	-i $EXT_IF -p udp -m multiport --sports $outgoing_udp -m state --state ESTABLISHED -j ACCEPT
+$iptables -A OUTPUT -o $EXT_IF -p udp -m multiport --dports $outgoing_udp -m state --state NEW,ESTABLISHED -j ACCEPT
+$iptables -A INPUT 	-i $EXT_IF -p udp -m multiport --sports $outgoing_udp -m state --state ESTABLISHED -j ACCEPT
 
 ### Make sure to drop bad packages
 echo "-> drop bad packages"
-$IPT -A INPUT -f -j DROP # Drop packages with incoming fragments
-$IPT -A INPUT -p tcp --tcp-flags ALL ALL -j DROP # Drop incoming malformed XMAS packets
-$IPT -A INPUT -p tcp --tcp-flags ALL NONE -j DROP # Drop all NULL packets
-$IPT -A INPUT -p tcp ! --syn -m state --state NEW -j DROP # Drop all new connection are not SYN packets
+$iptables -A INPUT -f -j DROP # Drop packages with incoming fragments
+$iptables -A INPUT -p tcp --tcp-flags ALL ALL -j DROP # Drop incoming malformed XMAS packets
+$iptables -A INPUT -p tcp --tcp-flags ALL NONE -j DROP # Drop all NULL packets
+$iptables -A INPUT -p tcp ! --syn -m state --state NEW -j DROP # Drop all new connection are not SYN packets
 
 ### ICMP (PING) - Ping flood projection 1 per second
 echo "-> allow ping"
-$IPT -A INPUT -p icmp -m limit --limit 5/s -j ACCEPT
-$IPT -A INPUT -p icmp -j DROP
+$iptables -A INPUT 	-p icmp -m limit --limit 5/s -j ACCEPT
+$iptables -A OUTPUT -p icmp -m limit --limit 5/s -j ACCEPT
+$iptables -A INPUT 	-p icmp -j DROP
+$iptables -A OUTPUT	-p icmp -j DROP
 
 ### Log all the rest before dropping
-$IPT -A INPUT   -j LOG --log-prefix "IN: "
-$IPT -A INPUT   -j DROP
-$IPT -A OUTPUT  -j LOG --log-prefix "OU: "
-$IPT -A OUTPUT  -j DROP
-$IPT -A FORWARD -j LOG --log-prefix "FW: "
-$IPT -A FORWARD -j DROP
+$iptables -A INPUT   -j LOG --log-prefix "IN: "
+$iptables -A INPUT   -j DROP
+$iptables -A OUTPUT  -j LOG --log-prefix "OU: "
+$iptables -A OUTPUT  -j DROP
+$iptables -A FORWARD -j LOG --log-prefix "FW: "
+$iptables -A FORWARD -j DROP
 
 ### End
 echo "DONE."
